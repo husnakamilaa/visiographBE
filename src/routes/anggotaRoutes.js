20c64eb981d59422ad9272012cce0cc63565e485
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-01-15 17:53:25 +0700

    feat: menambahkan route anggota

--- src/routes/anggotaRoutes.js
+++ src/routes/anggotaRoutes.js
@@ -3,22 +3,12 @@ const express = require("express");
 const router = express.Router();
 const anggotaController = require("../controllers/anggotaController");
 
-// CREATE
+
 router.post("/", authMiddleware, anggotaController.createAnggota);
-
-// READ
 router.get("/", authMiddleware, anggotaController.getAllAnggota);
-
-// SEARCH
 router.get("/search", authMiddleware, anggotaController.searchAnggota);
-
-// GET by ID
 router.get("/:id", authMiddleware, anggotaController.getAnggotaById);
-
-// UPDATE
 router.put("/:id", authMiddleware, anggotaController.updateAnggota);
-
-// DELETE
 router.delete("/:id", authMiddleware, anggotaController.deleteAnggota);
 
 module.exports = router;
